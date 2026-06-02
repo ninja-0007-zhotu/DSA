a6c0ddd714c0fbff026459046886b4e1a6620a00
ninja-0007-zhotu <anujsajankar005@gmail.com>
2026-06-03 01:19:30 +0530

    Time: 28 ms (40.25%) | Memory: 13.1 MB (82.31%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1001][1001];
+
+    int lcs(string &s1, string &s2, int n, int m){
+        if(n==0 || m==0)
+            return 0;
+
+        if(dp[n][m] != -1)
+            return dp[n][m];
+
+        if(s1[n-1] == s2[m-1]){
+            return dp[n][m] = 1 + lcs(s1,s2,n-1,m-1);
+        }
+
+        return dp[n][m] = max(
+            lcs(s1,s2,n-1,m),
+            lcs(s1,s2,n,m-1)
+        );
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(dp,-1,sizeof(dp));
+        return lcs(text1,text2,text1.size(),text2.size());
+    }
+};
