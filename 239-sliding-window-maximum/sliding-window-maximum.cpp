ce6c3514630822fc769f404bce166a616ae3a9d2
ninja-0007-zhotu <anujsajankar005@gmail.com>
2026-06-07 12:57:39 +0530

    Time: 61 ms (17.55%) | Memory: 153.2 MB (14.57%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> result;
+
+        int n = nums.size();
+
+        for(int i = 0; i<n; i++) {
+            
+            while(!pq.empty() && pq.top().second <= i-k)
+                pq.pop();
+            
+            pq.push({nums[i], i}); 
+
+            if(i >= k-1)
+                result.push_back(pq.top().first);
+        }
+        return result;
+    }
+};
