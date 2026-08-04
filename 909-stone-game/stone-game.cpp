df523c9e1db294394d2f21c40bac28cbbb4bb7ec
anuj Sajankar <anujsajankar005@gmail.com>
2026-08-05 00:37:54 +0530

    Time: 4 ms (40.42%) | Memory: 10.7 MB (42.59%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+
+        int n = piles.size();
+
+        vector<int> dp(piles);
+
+        // Current interval length.
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+
+                // Pick left or right pile.
+                dp[i] = max(
+                    piles[i] - dp[i + 1],
+                    piles[j] - dp[i]
+                );
+            }
+        }
+
+        return dp[0] > 0;
     }
 };
