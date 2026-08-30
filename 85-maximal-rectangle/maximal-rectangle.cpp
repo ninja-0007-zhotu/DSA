b8fd23a6c8f735c7f3a6eb30e9016b8d2573bbc1
anuj Sajankar <anujsajankar005@gmail.com>
2026-08-31 02:05:10 +0530

    Time: 8 ms (54.74%) | Memory: 19 MB (80.12%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    int hist(vector<int>& h) {
+        stack<int> st;
+        int ans = 0;
+        int n = h.size();
+
+        for (int i = 0; i <= n; i++) {
+
+            int curr = (i == n ? 0 : h[i]);
+
+            while (!st.empty() && h[st.top()] > curr) {
+
+                int height = h[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                ans = max(ans, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> height(m, 0);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+
+            ans = max(ans, hist(height));
+        }
+
+        return ans;
+    }
+};
