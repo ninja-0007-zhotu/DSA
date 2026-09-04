a9836316c5ff68184584811c410ecf4e46f728b9
anuj Sajankar <anujsajankar005@gmail.com>
2026-09-05 02:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.21%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>>result(m,vector<int>(n));
+
+           for(int i = 0; i < n; i++){ 
+                result[0][i] = 1;
+           }
+           for(int j = 0; j < m; j++){  
+                result[j][0] = 1;
+           }
+
+           for(int i = 1; i < m; i++){
+              for(int j = 1; j < n; j++){
+                result[i][j] = result[i-1][j] + result[i][j-1];
+              }
+           }
+           int ans = result[m-1][n-1];
+           return ans;
+    }
+};
