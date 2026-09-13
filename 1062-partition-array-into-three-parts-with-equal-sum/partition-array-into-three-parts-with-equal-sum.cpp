c32b5c58ca652cfebc6a94805881763f37fe1702
anuj Sajankar <anujsajankar005@gmail.com>
2026-09-14 03:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (78.89%) - LeetSync

---
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        int total = accumulate(arr.begin(), arr.end(), 0);
+
+        if (total % 3 != 0)
+            return false;
+
+        int target = total / 3;
+
+        long long pref = 0;
+        int parts = 0;
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            pref += arr[i];
+
+            if (pref == target) {
+                parts++;
+                pref = 0;
+            }
+
+            if (parts == 2)
+                return true;
+        }
+
+        return false;
+    }
+};
